82f95bdee99185724e8140c07a9eaa51861f3a14
EC2 Default User <ec2-user@ip-172-31-81-4.ec2.internal>
2021-05-01 11:32:01 +0000

    fix-all

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+ before_action :ensure_correct_user, only: [:edit, :destroy, :update]
 
   def show
     @book = Book.find(params[:id])
@@ -47,6 +48,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    @user = Book.find(params[:id]).user
+    unless @user == current_user
+      redirect_to books_path
+    end
   end
 
 end
